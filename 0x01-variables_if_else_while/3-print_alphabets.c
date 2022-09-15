6cb326718f221a8796e916d6cc2885a1e4499935
kahigamadeit <kahingakimani@gmail.com>
2022-09-15 01:27:05 -0700

    hello

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
  * main -Entry point
  * Description: prints alphabet in lowercase then uppercase
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 int ch;
