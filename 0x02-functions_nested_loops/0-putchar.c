a5501ba9b298fe53a524e641b79a6d6e99593c03
kahigamadeit <kahingakimani@gmail.com>
2022-09-15 12:34:21 -0700

    kijana

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('P');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * main - 
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{char i;
+char h;
+for (h = 0; h <= 9; h++)
+{for (i = 'a'; i <= 'z'; i++)
+{putchar(i);
+}putchar('\n');
+}
+}
