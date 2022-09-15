16eee2c57d8bef7574bcc6c97ba2a3e7a4552ba3
kahigamadeit <kahingakimani@gmail.com>
2022-09-15 11:49:48 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
- *
+ * Description: use printf
  * Return: always 0
  */
+
 int main(void)
-{printf("with proper grammer, but the outcome is a piece of art,\n");
+{
+printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
