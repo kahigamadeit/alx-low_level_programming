8c92ece93f31e06b016ef6995a351ce9dfe862cf
kahigamadeit <kahingakimani@gmail.com>
2022-09-22 14:08:33 -0700

    lola

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppers a lower
+ * @s: character to make upper
+ * Return: return value of upper
+ */
+
+char *string_toupper(char *str)
+{
+int h;
+for (h = 0; str[h] != '\0'; h++)
+if (str[h] >= 'a' && str[h] <= 'z')
+str[h] = str[h] - 32;
+return (str);
+}
