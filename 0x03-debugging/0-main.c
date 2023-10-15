481be9683a3fc7687ddb4ebc573f1c921ba11403
kahigamadeit <kahingakimani@gmail.com>
2023-10-14 18:19:01 -0700

    t 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Test function for positive or negative
+ * main: Test function for positive or negative
  * Return: 0
  */
 int main(void)
@@ -9,4 +9,13 @@ int i;
 i = 0;
 positive_or_negative(i);
 return (0);
+}
+void positive_or_negative(int n)
+{
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
 }
