4747a640b3cf9ebd9aeb6f3ffb5390242abd8ba5
kahigamadeit <kahingakimani@gmail.com>
2023-10-14 18:23:21 -0700

    tr 3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,12 +10,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-void positive_or_negative(int n)
-{
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is zero\n", n);
-}
