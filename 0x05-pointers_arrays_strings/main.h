a7dcff1a1ce50e1de33e79159359c0a6c5115ffe
kahigamadeit <kahingakimani@gmail.com>
2023-10-17 19:10:29 -0700

    tk 2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * void prototypes()
  * int prototypes()
@@ -16,4 +17,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 #endif /*MAIN_H*/
