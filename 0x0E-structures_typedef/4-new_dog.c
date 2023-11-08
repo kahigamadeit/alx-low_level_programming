393b8beb6632302760ef408729ad09de895435bf
kahigamadeit <kahingakimani@gmail.com>
2023-11-07 18:34:50 -0800

    qa

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,44 +1,46 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * new_dog - a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: pointer to a new dog
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-unsigned int nl, ol, i;
-dog_t *dog;
-if (name == NULL || owner == NULL)
-return (NULL);
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
-for (nl = 0; name[nl]; nl++)
-;
-nl++;
-dog->name = malloc(nl * sizeof(char));
-if (dog->name == NULL)
+int i = 0, j = 0, k;
+dog_t *smart;
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+smart = malloc(sizeof(dog_t));
+if (smart == NULL)
 {
-free(dog);
+free(smart);
 return (NULL);
 }
-for (i = 0; i < nl; i++)
-dog->name[i] = name[i];
-dog->age = age;
-for (ol = 0; owner[ol]; ol++)
-;
-ol++;
-dog->owner = malloc(ol * sizeof(char));
-if (dog->owner == NULL)
+smart->name = malloc(i * sizeof(smart->name));
+if (smart->name == NULL)
 {
-free(dog->name);
-free(dog);
+free(smart->name);
+free(smart);
 return (NULL);
 }
-for (i = 0; i < ol; i++)
-dog->owner[i] = owner[i];
-return (dog);
+for (k = 0; k <= i; k++)
+smart->name[k] = name[k];
+smart->age = age;
+smart->owner = malloc(j * sizeof(smart->owner));
+if (smart->owner == NULL)
+{
+free(smart->owner);
+free(smart->name);
+free(smart);
+return (NULL);
+}
+for (k = 0; k <= j; k++)
+smart->owner[k] = owner[k];
+return (smart);
 }
