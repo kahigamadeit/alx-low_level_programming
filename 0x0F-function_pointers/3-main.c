ee62d2ab959c9bcb1c096f5d1f48a96e60473490
kahigamadeit <kahingakimani@gmail.com>
2023-11-09 18:00:04 -0800

    lst chg

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 /**
- * main - calculates the values of two numbers
- * @argc: argument count.
- * @argv: argument vector.
+ * main - This calculates the values of two numbers
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
  *
- * Return: 0 always
+ * Return: 0 if succesfful
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 int (*f)(int a, int b);
 int a, b;
@@ -22,17 +22,12 @@ if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 printf("Error\n");
 exit(100);
 }
-t = get_op_func(argv[2]);
-if (t == NULL)
+f = get_op_func(argv[2]);
+if (f == NULL)
 {
 printf("Error\n");
 exit(99);
 }
 printf("%d\n", f(a, b));
-return (0);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-printf("%d\n", t(a, b));
 return (0);
 }
