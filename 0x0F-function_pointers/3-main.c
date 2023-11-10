55de77142ac905ff6fc1c55519309f0d4fb0189f
kahigamadeit <kahingakimani@gmail.com>
2023-11-09 17:56:58 -0800

    chgs

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 /**
- * main - gets arguements from cli
+ * main - calculates the values of two numbers
  * @argc: argument count.
  * @argv: argument vector.
  *
  * Return: 0 always
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
 int (*f)(int a, int b);
 int a, b;
@@ -22,12 +22,17 @@ if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 printf("Error\n");
 exit(100);
 }
-f = get_op_func(argv[2]);
-if (f == NULL)
+t = get_op_func(argv[2]);
+if (t == NULL)
 {
 printf("Error\n");
 exit(99);
 }
 printf("%d\n", f(a, b));
+return (0);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+printf("%d\n", t(a, b));
 return (0);
 }
