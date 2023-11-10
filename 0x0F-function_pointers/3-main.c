52b193dda151384016581567317500f2e99bdcb0
kahigamadeit <kahingakimani@gmail.com>
2023-11-09 17:37:38 -0800

    lst 1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,28 +6,28 @@
  *
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+int (*f)(int a, int b);
 int a, b;
-int (*operation)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (argv[2][1])
-{
-printf("Error\n");
-exit(99);
-}
-operation = get_op_func(argv[2]);
-if (operation == NULL)
-{
-printf("Error\n");
-exit(99);
-}
 a = atoi(argv[1]);
 b = atoi(argv[3]);
-printf("%d\n", operation(a, b));
+if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+f = get_op_func(argv[2]);
+if (f == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", f(a, b));
 return (0);
 }
