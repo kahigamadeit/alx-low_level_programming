cf3a1cab058b1954a67d72fac82bc6494b253127
kahigamadeit <kahingakimani@gmail.com>
2024-01-12 03:16:06 -0800

    lk

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * Print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ *  @n: the int to check
+ *  Return: 1 and prints + if n is greater than zero
+ *  0 and prints 0 if n is zero
+ *  -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
